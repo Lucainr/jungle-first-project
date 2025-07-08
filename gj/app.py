ed66b724cdf4a7dfd4de9d931bb02ba4454c0e1f
pgj1394 <whrpwls1234@hanmail.net>
2025-07-08 10:18:30 +0000

    JWT Login

--- gj/app.py
+++ gj/app.py
@@ -1,4 +1,10 @@
+import jwt # jwt 라이브러리 임포트
+import datetime # 유효기간 설정을 위한 라이브러리
+from pymongo import MongoClient # <--- 이 줄을 꼭 추가해줘!
 from flask import Flask, render_template, request, jsonify # request, jsonify 추가
+
+# JWT를 위한 비밀 키. 아무 문자열이나 괜찮지만, 실제 서비스에선 복잡하게 만들어야 해!
+SECRET_KEY = 'JUNGLE'
 
 # MongoDB 연결 설정
 client = MongoClient('mongodb://test:test@15.164.170.54', 27017)
@@ -13,6 +19,37 @@ def home():
 
 ## API 역할을 하는 부분
 
+# 로그인 API
+@app.route('/api/login', methods=['POST'])
+def SignIn():
+    # 1. 클라이언트로부터 데이터 받아오기
+    id_receive = request.form.get('id_give')
+    password_receive = request.form.get('password_give')
+
+    # 2. 비밀번호 암호화
+    import hashlib
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+
+    # 3. DB에서 아이디, 암호화된 비밀번호가 일치하는 사용자가 있는지 확인
+    result = db.users.find_one({'id': id_receive, 'password': password_hash})
+
+    # 4. 사용자가 존재하면, JWT 생성
+    if result is not None:
+        # JWT에 담을 정보 (payload)
+        # 'id'는 사용자를 식별할 수 있는 정보, 'exp'는 토큰의 만료 시간
+        payload = {
+            'id': id_receive,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)  # 1시간 동안 유효?
+        }
+        # JWT 생성
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')   # payload와 SECRET_KEY를 합쳐서 암호화된 토큰(문자열)을 인코딩
+
+        # 5. 생성한 토큰을 클라이언트에게 보내주기
+        return jsonify({'result': 'success', 'token': token})
+    else:
+        # 사용자가 없다면, 실패 메시지 전송
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
 # 회원가입 API
 @app.route('/api/signup', methods=['POST'])
 def SignUp():
@@ -22,7 +59,7 @@ def SignUp():
     password_receive = request.form.get('password_give')
     email_receive = request.form.get('email_give')
 
-    # 2. 비밀번호 암호화 (절대!!! 비밀번호는 그대로 저장하면 안돼)
+    # 2. 비밀번호 암호화
     import hashlib
     password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
 
